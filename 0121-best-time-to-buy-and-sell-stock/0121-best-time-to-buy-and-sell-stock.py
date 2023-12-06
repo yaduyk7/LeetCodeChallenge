549d4adc594bcff7ba781bd56707084b88b83cba
yaduyk7 <152846587+yaduyk7@users.noreply.github.com>
2023-12-06 19:05:50 +0100

    Time: 772 ms (56.51%), Space: 23.1 MB (7.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        profit_list=[0] #if 'min_price' is getting updated after every loop(i.e the prices list is sorted in decending order, then no profit is possible, in that case 0 has to be returned. And if there is no profit, no element will get appended to 'profit_list'. So, if there is a default zero in 'profit_list', finally when max() is called it will return 0
+        profit_list=[0]
         min_price=prices[0]
         for i in range(1,len(prices)):
             if(prices[i]<min_price):
