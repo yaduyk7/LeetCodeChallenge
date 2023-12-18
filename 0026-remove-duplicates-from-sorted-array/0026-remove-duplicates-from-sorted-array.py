a208e05a4db78e5ab307c75432ccbad983b2572f
yaduyk7 <152846587+yaduyk7@users.noreply.github.com>
2023-12-18 17:56:51 +0100

    Time: 64 ms (36.75%), Space: 14.4 MB (86.55%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -7,10 +7,9 @@ class Solution(object):
         pt1=0
         pt2=1
         while(pt2<len(nums)):
-            if (nums[pt1]==nums[pt2]):
-                pass
-            else:
+            if (nums[pt1]!=nums[pt2]):
                 pt1+=1
                 nums[pt1],nums[pt2]=nums[pt2],nums[pt1]
             pt2+=1
-        return pt1+1
+        k=pt1+1 #pt1 will stop at last unique element. So number of unique elements will be pt1+1 bcoz pt1 starts from 0
+        return k
